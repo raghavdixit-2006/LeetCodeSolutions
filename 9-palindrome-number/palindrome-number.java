6556dc0d292f2ade0ccc433b4546e9ddab2ceadc
Raghav Dixit <raghav0667.becse24@chitkara.edu.in>
2026-07-21 09:48:45 +0530

    Time: 5 ms (80.76%) | Memory: 45.7 MB (94.60%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,7 +1,22 @@
 class Solution {
-    public boolean isPalindrome(int x) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(x);
-        return (sb.toString().equals(sb.reverse().toString()));
+    public boolean isPalindrome(int x) 
+    {
+        if(x < 0)
+        return false;
+
+        int rev = 0;
+        int rem = 0;
+         int temp = x;
+         while(x != 0)
+         {
+            rem = x%10;
+            rev = (rev*10) + rem;
+            x = x/10;
+         }
+         if(rev == temp)
+         return true;
+         else
+         return false;
+        
     }
 }
