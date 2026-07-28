b9a431478d8db79cabf435c21b4ef60f9c14e4ed
Raghav Dixit <raghav0667.becse24@chitkara.edu.in>
2026-07-28 12:12:46 +0530

    Time: 43 ms (14.06%) | Memory: 48.3 MB (23.69%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String smallestPalindrome(String s) {
+        int[] arr = new int[26];
+        int idx=-1;
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            arr[ch-'a'] += 1;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            if(arr[i]%2 != 0){
+                idx=i;
+                arr[i]--;
+            }
+            if(arr[i]==0) continue;
+            char ch = (char) ('a' + i);
+
+            if(arr[i]%2 == 0){for(int j=0; j < arr[i]/2; j++){
+                sb.append(ch);
+            }}
+            
+        }
+
+        if(idx!=-1){for (int i = 0; i <1; i++) {
+            char ch = (char) ('a' + idx);
+            sb.append(ch);
+        }}
+//        for (int i = 25; i >= 0; i--) {
+//            if(arr[i]>0){
+//                char ch = (char) ('a' + i);
+////                sb.append(ch);
+//            }
+//        }
+        for (int i = 25; i >= 0; i--) {
+            if(arr[i]==0) continue;
+            if(arr[i]%2 != 0) continue;
+            char ch = (char) ('a' + i);
+            for(int j=0; j < arr[i]/2; j++){
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
