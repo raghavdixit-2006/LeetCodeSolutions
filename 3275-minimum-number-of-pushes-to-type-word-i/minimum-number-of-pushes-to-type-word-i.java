01db5cc9526f7a3e612a961cf572c58ceffee180
Raghav Dixit <raghav0667.becse24@chitkara.edu.in>
2026-07-30 10:03:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (28.23%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -1,9 +1,8 @@
 class Solution {
     public int minimumPushes(String word) {
-        System.out.println(word.length());
         if(word.length()<=8) return word.length();
-        if(word.length()<=16)return 8+(word.length()-8)*(1*2);
-        if(word.length()<=24)return (word.length()-8)*(1*3);
+        else if(word.length()<=16)return 8+(word.length()-8)*(1*2);
+        else if(word.length()<=24)return (word.length()-8)*(1*3);
         return (word.length()*2)+((word.length()-24)*2);
     }
 }
