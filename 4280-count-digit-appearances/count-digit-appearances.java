97ad33981d0c103ff326cc7000e17c2cf505f029
Raghav Dixit <raghav0667.becse24@chitkara.edu.in>
2026-08-10 11:58:20 +0530

    Time: 4 ms (97.83%) | Memory: 47.1 MB (18.04%) - LeetSync

---
+++ 4280-count-digit-appearances/count-digit-appearances.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countDigitOccurrences(int[] nums, int digit) {
+        int count=0;
+        for(int i=0; i <nums.length; i++){
+            int n = nums[i];
+            while(n != 0){
+                if(n%10==digit){
+                    count++;
+                }
+                n /= 10;
+            }
+        }
+        return count;
+    }
+}
