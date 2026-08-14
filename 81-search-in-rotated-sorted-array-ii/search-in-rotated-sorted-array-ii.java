531f40ea8a46999e7de39c1df1fa7e8b06649c0c
Raghav Dixit <raghav0667.becse24@chitkara.edu.in>
2026-08-14 14:50:14 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (29.41%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int i=0;
+        int j=nums.length-1;
+        while (i <= j) {
+            if (nums[i] == target) return true;
+            if (nums[j] == target) return true;
+
+            i++;
+            j--;
+        }
+        return false;
+    }
+}
