c6dc1e31b3664af4f6ba3121a1ed23994a411493
Raghav Dixit <raghav0667.becse24@chitkara.edu.in>
2026-08-19 11:20:56 +0530

    Time: 1 ms (100.00%) | Memory: 47.9 MB (98.60%) - LeetSync

---
+++ 4271-find-the-degree-of-each-vertex/find-the-degree-of-each-vertex.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] findDegrees(int[][] matrix) {
+        int[] ans = new int[matrix.length];
+        for(int i = 0; i < matrix.length; i++){
+            int sum = 0;
+            for(int j = 0; j < matrix[0].length; j++){
+                sum += matrix[i][j];
+            }
+            ans[i] = sum;
+        }
+        return ans;
+    }
+}
